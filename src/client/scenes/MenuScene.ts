c5bbcb9975f74d7b07f8d1dcac37c5808fb9f2ef
SamJ-Inf <samuel.justin.behringer@gmail.com>
2023-05-12 09:27:43 +0200

    Almost finished the menus

--- src/client/scenes/MenuScene.ts
+++ src/client/scenes/MenuScene.ts
@@ -1,5 +1,3 @@
-
-
 export class MenuScene extends Phaser.Scene {
     constructor() {
         super({
@@ -16,10 +14,11 @@ export class MenuScene extends Phaser.Scene {
        
         
         const roomCodeInput = this.add.dom(75, 105).createFromCache("Room code")
-        let makeGame = this.add.text(100, 50, "Click here to create a game")
-        let joinGame = this.add.text(100, 150, "Enter a room key here, to join a game")
+        let makeGame = this.add.text(100, 150, "Click here to create a game")
+        let joinGame = this.add.text(100, 50, "Enter a room key here, to join a game")
 
-
+        jQuery('.Username').hide();
+        jQuery('.Room_code').show();
 
       
         
--- src/client/scenes/StartScene.ts
+++ src/client/scenes/StartScene.ts
@@ -13,13 +13,16 @@ export class StartScene extends Phaser.Scene {
 
     create() {
         let text = this.add.text(100, 100, "Chess") 
-        const username = this.add.dom(75, 55).createFromCache('Username')
-        username.setVisible(false)
+       
+        
         let caption = this.add.text(100, 300, "Press Enter to start")
         this.input.keyboard.on('keydown-ENTER', function() {
             console.log('From StartScene to MenuScene')
             this.scene.start('MenuScene')
         }, this)
+
+        jQuery('.Username').show();
+        jQuery('.Room_code').hide();
         
     }
 }
