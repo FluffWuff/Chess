db2cc3d419c827c2aa54f7789b98fe3dd5090c2e
SamJ-Inf <samuel.justin.behringer@gmail.com>
2023-05-19 09:25:37 +0200

    Finished Menu

--- src/client/scenes/MenuScene.ts
+++ src/client/scenes/MenuScene.ts
@@ -1,3 +1,5 @@
+import { Button } from "./Button.js";
+
 export class MenuScene extends Phaser.Scene {
     constructor() {
         super({
@@ -7,22 +9,19 @@ export class MenuScene extends Phaser.Scene {
     }
     preload() {
         this.load.html('Room code', 'index.html')
-        
     }
 
     create() {
-       
-        
 
-        let makeGame = this.add.text(100, 150, "Click here to create a game")
+
+
+        
         let joinGame = this.add.text(100, 50, "Enter a room key here, to join a game")
 
         jQuery('.Username').hide();
         jQuery('.Room_code').show();
 
-      
-        
+        let button = new Button(280, 150, 'Click here to create game', this, () => console.log('New game room opened'));
 
-    
-}
+    }
 }
